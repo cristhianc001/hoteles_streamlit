d5a5245983ce8da4eece9aae237253d2579e7da9
Cristhian Castro <cristhiancastro001@gmail.com>
2023-08-30 17:39:33 -0500

    api key como var. env

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import re
+import demoji
+import openai
+# import transformers
+# from transformers import pipeline
+
+st.image('https://d31uz8lwfmyn8g.cloudfront.net/Assets/logo-henry-white-lg.png')
+st.title('Titulo')
+st.markdown('***')
+st.sidebar.markdown('Prueba Streamlit')
+
---
+++ pages/01_🤖_Demo Clasificacion Reviews.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import re
+import demoji
+import openai
+
+openai.api_key = st.secrets["API_KEY"]
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def cleaning_text(text):
+# TRATAR TRADUCCIONES EN REVIEWS, BORRA EL STRING "(ORIGINAL)" Y TODO A SU DERECHA
+  keyword = "(Original)"
+  keyword_index = text.find(keyword) # Encontrar la posición de la palabra clave
+  if keyword_index != -1:
+      text = text[:keyword_index + len(keyword)] # Eliminar todo después de la palabra clave
+
+  text = text.replace("(Original)", "")
+  text = text.replace("(Translated by Google)", "")
+
+# TRATAR EMOJIS
+  text = demoji.replace_with_desc(text, ' ') # reemplaza los emojis con palabras mas un espacio entre si
+
+# BORRAR SALTOS DE LINEA
+  text = text.replace("\n", " ")
+# BORRAR CARACTERES NO ALFANUMERICOS
+  text = re.sub(r'[^\w\s]', '', text)
+# BORRAR ESPACIOS AL PRINCIPIO Y AL FINAL Y CONVERTIR A MINUSCULAS
+  text = text.strip()
+  text = text.lower()
+# DESPUES DE LIMPIADO VERIFICAR SI HAY CARACTERES O NO, SI NO HAY CONVERTIRLOS EN NULO
+  if not text:
+      return None
+  return text
+
+# @st.cache(hash_funcs={pipeline: lambda _: None, tokenizers.Tokenizer: lambda _: None})
+# def load_model():
+#     zsc_mDeBERTa = pipeline("zero-shot-classification", model = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7")
+#     return zsc_mDeBERTa
+# model = load_model()
+
+# candidate_labels = ["cleaning and bugs", "room and comfort", "staff and services", "food and drinks", "money"]
+
+
+# def zero_shot_classification(text, candidate_labels, model):
+#     results = model(text, candidate_labels)
+#     predicted_labels = []
+
+#     # Obtener la etiqueta con el puntaje más alto
+#     max_score_label = max(results['scores'])
+#     max_score_index = results['scores'].index(max_score_label)
+#     max_score_label = results['labels'][max_score_index]
+
+#     threshold = 0.3  # Umbral de confianza para considerar una etiqueta
+#     for label, score in zip(results['labels'], results['scores']):
+#         if score > threshold:
+#             predicted_labels.append(label)
+
+#     # Si ninguna etiqueta pasa el umbral, usar la etiqueta con el mayor score
+#     if not predicted_labels:
+#         predicted_labels.append(max_score_label)
+
+#     return predicted_labels
+
+# Crear un cuadro de texto y obtener el texto ingresado por el usuario
+input_text = st.text_input("Write a review:")
+
+# Verificar si el usuario ha ingresado texto y luego llamar a la función
+if input_text:
+    input_text = cleaning_text(input_text)
+    resultado =  get_completion(
+        f""" 1. Give me the overall sentiment of the next review, the response can be 1 for positive, -1 for negative or 0 for neutral.
+            2. Give me the sentiment of the next categories of the review: room, guest service, cleaning and breakfast.
+            The response must be 1 for positive, -1 for negative or 0 neutral.
+            3. The output must be in JSON format and the keys must be 'overall_sentiment', 'room_sentiment', 'guest_service_sentiment','cleaning_sentiment' and 'breakfast_sentiment'.
+            \ ```{input_text}``` """)
+        
+    st.write("Sentiment Analysis:", resultado)
