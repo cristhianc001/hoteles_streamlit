91140a59afcff56c741fe58441e541bbbaa3cadc
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-02 01:54:23 -0500

    filtro fecha

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
 df = pd.read_parquet("data/df_transformed.parquet")
+df["date"] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
--- pages/02_📈_Datos de Hoteles.py
+++ pages/02_📈_Datos de Hoteles.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-from data import df
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    df = pd.read_parquet("data/df_transformed.parquet")
+    df["date"] = [x.date() for x in df["date"]]
+    return df
+df = load_data()
 
 # Lista de opciones para la lista desplegable
 opciones = ["Ramada Plaza by Wyndham Orlando Resort & Suites Intl Drive", "Ramada by Wyndham New York Times Square West",
@@ -11,7 +18,15 @@ opciones = ["Ramada Plaza by Wyndham Orlando Resort & Suites Intl Drive", "Ramad
 # Crear una lista desplegable en la aplicación
 opcion_seleccionada = st.selectbox("Selecciona una opción:", opciones)
 
-df_filtrado = df[df['lodging_name'] == opcion_seleccionada]
+# Agregar filtros de fecha
+st.sidebar.header("Filtros de Fecha")
+fecha_minima = pd.to_datetime(min(df["date"]))
+fecha_maxima = pd.to_datetime(max(df["date"]))
+
+fecha_inicio = st.sidebar.date_input("Fecha de Inicio", min_value = fecha_minima, max_value = fecha_maxima, value=fecha_minima)
+fecha_fin = st.sidebar.date_input("Fecha de Fin", min_value = fecha_minima, max_value = fecha_maxima, value=fecha_maxima)
+
+df_filtrado = df[(df['lodging_name'] == opcion_seleccionada) & (df['date'] >= fecha_inicio) & (df['date'] <= fecha_fin)]
 
 text = ' '.join(df_filtrado['transformed'])
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
@@ -21,5 +36,7 @@ st.image(wordcloud.to_array())
 
 # Mostrar las opiniones correspondientes al hotel seleccionado
 st.write("Opiniones para", opcion_seleccionada)
-st.write(df_filtrado['transformed'].tolist())
+st.write("Fecha de Inicio:", fecha_inicio)
+st.write("Fecha de Fin:", fecha_fin)
+st.write(df_filtrado['cleaned'].tolist())
 
