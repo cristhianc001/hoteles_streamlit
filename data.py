361eba6d48936a88b572695fad5208bdf208dd7e
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-04 15:34:42 -0500

    mapa

--- data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet("data/df_transformed.parquet")
-df["date"] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
---
+++ pages/03_🌏_ Visualización Geografica.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
+
+@st.cache_data
+def load_lodgings():
+    df_lodgings = pd.read_parquet("data/df_lodgings.parquet")
+    return df_lodgings
+
+df_lodgings = load_lodgings()
+
+# Lista de opciones para la lista desplegable
+opciones = ["Ramada Plaza by Wyndham Orlando Resort & Suites Intl Drive", "Ramada by Wyndham New York Times Square West",
+            "Ramada Plaza by Wyndham West Hollywood Hotel & Suites",  "Ramada by Wyndham Reno Hotel & Casino", 
+            "Ramada by Wyndham Houston Intercontinental Airport South", "Ramada by Wyndham Houston Intercontinental Airport East", "Nacional"]
+
+opcion_seleccionada = st.selectbox("Selecciona una opción:", opciones)
+
+if opcion_seleccionada == "Nacional":
+    # Crear un clúster de marcadores para hoteles seleccionados
+    mc_selected = MarkerCluster()
+    mc_unselected = MarkerCluster()
+
+    map_center = [df_lodgings['latitude'].mean(), df_lodgings['longitude'].mean()]
+
+    m_3 = folium.Map(location=map_center, tiles='cartodbpositron', zoom_start=4.5)
+
+    for idx, row in df_lodgings.iterrows():
+        if not pd.isna(row['longitude']) and not pd.isna(row['latitude']):
+            # Verificar si el hotel está en la lista de hoteles seleccionados
+            if row['lodging_name'] in opciones:
+                # Agregar un marcador con estilo diferente para hoteles seleccionados
+                marcador = folium.Marker(
+                    location=[row['latitude'], row['longitude']],
+                    tooltip=row['lodging_name'],  # tooltip para que muestre el nombre cuando se pose
+                    icon=folium.Icon(color='red', icon='info-sign')  # Cambia el color del icono a rojo
+                )
+                marcador.add_to(mc_selected)
+            else:
+                # Agregar un marcador normal para hoteles no seleccionados
+                marcador = folium.Marker(
+                    location=[row['latitude'], row['longitude']],
+                    tooltip=row['lodging_name']  # tooltip para que muestre el nombre cuando se pose
+                )
+                marcador.add_to(mc_unselected)
+
+    # Agregar los clústeres de marcadores al mapa principal
+    m_3.add_child(mc_selected)
+    m_3.add_child(mc_unselected)
+
+    st.markdown("### Mapa de hoteles")
+    st.write("Haga clic en los marcadores para obtener información.")
+    folium_static(m_3)
