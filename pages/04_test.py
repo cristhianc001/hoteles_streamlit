54bafcc2b332dc3524cd792c9d0d6ce425988d10
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-07 15:57:28 -0500

    filtro fecha

--- pages/04_test.py
+++ pages/04_test.py
@@ -5,7 +5,7 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import pandas as pd
 import nltk
-import random
+from datetime import datetime
 
 ########### TITULO
 st.title('Revision de reseñas')
@@ -16,6 +16,7 @@ st.markdown('***')
 opciones = ["Ramada Plaza by Wyndham Orlando Resort & Suites Intl Drive", "Ramada by Wyndham New York Times Square West",
             "Ramada Plaza by Wyndham West Hollywood Hotel & Suites",  "Ramada by Wyndham Reno Hotel & Casino", 
             "Ramada by Wyndham Houston Intercontinental Airport East"]
+
 # Hotels de competencia
 lista_id_NV = [855,851]
 lista_id_FL = [1010,1062]
@@ -26,6 +27,7 @@ lista_id_TX = [1521,1517]
 @st.cache_data
 def load_data():
     df = pd.read_parquet("data/reviews-model.parquet")
+    df["date"] = [datetime.strptime(x, "%Y-%m-%d").date() for x in df["date"]]
     return df
 df = load_data()
 
@@ -50,6 +52,14 @@ def WC(df_wc):
     return wordcloud  # Devuelve directamente el objeto WordCloud
 
 ########### CONTENIDO
+# Agregar filtros de fecha
+st.sidebar.header("Filtros de Fecha")
+fecha_minima = pd.to_datetime(min(df["date"]))
+fecha_maxima = pd.to_datetime(max(df["date"]))
+
+fecha_inicio = st.sidebar.date_input("Fecha de Inicio", min_value = fecha_minima, max_value = fecha_maxima, value=fecha_minima)
+fecha_fin = st.sidebar.date_input("Fecha de Fin", min_value = fecha_minima, max_value = fecha_maxima, value=fecha_maxima)
+
 opcion_sentimiento = st.sidebar.radio("Seleccione una opcion de sentimiento:", ["Positivo", "Negativo", "Total"])
 if opcion_sentimiento == "Positivo":
     sentimiento = 1
@@ -87,24 +97,24 @@ elif opcion_hotel == "Ramada by Wyndham Houston Intercontinental Airport East":
     hotel = 1520
     lista_comp = lista_id_TX
 
+# Hoteles de Ramada
 if sentimiento is None:
-    df_filtrado = df[(df['lodging_id'] == hotel)]
+    df_filtrado = df[(df['lodging_id'] == hotel) & (df['date'] >= fecha_inicio) & (df['date'] <= fecha_fin)]
 else:
-    df_filtrado = df[(df['lodging_id'] == hotel) & (df[categoria] == sentimiento)]
-
+    df_filtrado = df[(df['lodging_id'] == hotel) & (df[categoria] == sentimiento)  & (df['date'] >= fecha_inicio) & (df['date'] <= fecha_fin)]
+# Hoteles de la competencia
 if sentimiento is None:
-    df_filtrado_comp = df[df['lodging_id'].isin(lista_comp)]
+    df_filtrado_comp = df[df['lodging_id'].isin(lista_comp)  & (df['date'] >= fecha_inicio) & (df['date'] <= fecha_fin)]
 else:
-    df_filtrado_comp = df[(df['lodging_id'].isin(lista_comp)) & (df[categoria] == sentimiento)]
+    df_filtrado_comp = df[(df['lodging_id'].isin(lista_comp)) & (df[categoria] == sentimiento)  & (df['date'] >= fecha_inicio) & (df['date'] <= fecha_fin)]
 
 # Mostrar la WordCloud
-st.write("Nube de palabras para la categoria: ", opcion_categoria , " con sentimientos: ", opcion_sentimiento , " para el hotel: ", opcion_hotel)
+st.markdown(f"Nube de palabras para la categoría: **{opcion_categoria}** con sentimientos: **{opcion_sentimiento}** para el hotel: ***{opcion_hotel}***")
 st.image(WC(df_filtrado).to_array())
 
-st.write("Nube de palabras para la categoria: ", opcion_categoria , " con sentimientos: ", opcion_sentimiento , " para la competencia")
+st.markdown(f"Nube de palabras para la categoría: **{opcion_categoria}** con sentimientos: **{opcion_sentimiento}** para la ***competencia***")
 st.image(WC(df_filtrado_comp).to_array())
 
 # Mostrar las opiniones correspondientes al hotel seleccionado
-reviews_filtradas = df_filtrado[df_filtrado[categoria] != 0]
 st.write("Opiniones para", opcion_hotel)
-st.write(reviews_filtradas['review'].tolist())
+st.write(df_filtrado['review'].tolist())
