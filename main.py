93731c8b908693500ffb60f4718d9d889a1118a6
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-07 16:33:04 -0500

    icono corregido 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # st.title('Sistema de Analisis de Reseñas')
-st.sidebar.image("https://github.com/cristhianc001/hoteles_streamlit/blob/main/img/icon.png",caption="Developed and Maintaned by: Latam Data Consultores")
+st.sidebar.image("https://raw.githubusercontent.com/cristhianc001/hoteles_streamlit/main/img/icon.png",caption="Developed and Maintaned by: Latam Data Consultores")
 
 st.image('https://raw.githubusercontent.com/cristhianc001/Analisis-Sentimientos-Hoteles/main/img/1.jpg')
 st.markdown('***')
