64eaf46c39b98f7a60b591ddcb436fbe3560a455
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-08 01:25:54 -0500

    ramada logo2

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ st.sidebar.image("https://raw.githubusercontent.com/cristhianc001/hoteles_stream
 
 st.image('https://raw.githubusercontent.com/cristhianc001/Analisis-Sentimientos-Hoteles/main/img/1.jpg')
 st.markdown('***')
-st.image('https://1000marcas.net/wp-content/uploads/2021/06/Ramada-logo.png')
+# st.image('https://1000marcas.net/wp-content/uploads/2021/06/Ramada-logo.png')
 st.image('https://raw.githubusercontent.com/cristhianc001/hoteles_streamlit/main/img/ramada_logo.png')
 
