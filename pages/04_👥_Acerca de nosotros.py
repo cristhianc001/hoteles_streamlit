9f1121b1dff9c955e140080caf39ae55346fc5bd
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-15 17:28:28 -0500

    pagina de equipo

---
+++ pages/04_👥_Acerca de nosotros.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Acerca de nosotros')
+
+st.image('https://raw.githubusercontent.com/cristhianc001/hoteles_streamlit/main/img/team.png')
+
+st.markdown("***Data Analyst***: [Douglas Sanchez](https://www.linkedin.com/in/douglassanchezcasanova/)")
+
+st.markdown("***Data Engineers***: [Yaneth Ramirez]( https://www.linkedin.com/in/yanethramirez/), [Hugo Salazar](https://www.linkedin.com/in/hasalazars/)")
+
+st.markdown("***Data Scientists***: [Cristhian Castro](https://www.linkedin.com/in/cristhiancastro/), [Rodrigo Moreira](https://www.linkedin.com/in/rcmoreg)")
