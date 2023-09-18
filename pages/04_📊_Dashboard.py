6e80d2f66fbe2043dbda996969a9274de2017e5d
Cristhian Castro <cristhiancastro001@gmail.com>
2023-09-18 13:25:06 -0500

    correccion foto team

--- pages/04_📊_Dashboard.py
+++ pages/04_📊_Dashboard.py
@@ -22,3 +22,48 @@ css = '''
 '''
 
 st.markdown(css, unsafe_allow_html=True)
+
+
+
+
+
+# import streamlit as st
+
+# # Define la métrica y su valor
+# metric_name = "Ingresos Mensuales"
+# metric_value = 8000  # Cambia este valor según tus necesidades
+# objetivo = 10000
+
+# # Calcula si se cumple el objetivo
+# cumple_objetivo = metric_value >= objetivo
+
+# # Define los colores para la tarjeta
+# color_fondo = "green" if cumple_objetivo else "red"
+# color_texto = "white" if cumple_objetivo else "black"
+
+# # Aplica estilos CSS condicionales
+# st.markdown(
+#     f"""
+#     <style>
+#     .kpi-card {{
+#         background-color: {color_fondo};
+#         color: {color_texto};
+#         padding: 20px;
+#         border-radius: 10px;
+#         text-align: center;
+#         font-size: 24px;
+#     }}
+#     </style>
+#     """
+#     ,
+#     unsafe_allow_html=True,
+# )
+
+# # Crea la tarjeta KPI
+# st.subheader("KPI Card")
+
+# # Muestra el valor de la métrica y el estado del objetivo
+# st.markdown(
+#     f'<div class="kpi-card">**{metric_name}**: ${metric_value:,.2f} </div>', # ({"" if cumple_objetivo else "No "}Cumple Objetivo) para poner aviso entre parentesis
+#     unsafe_allow_html=True,
+# )
